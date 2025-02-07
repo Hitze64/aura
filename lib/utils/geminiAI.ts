b25cc84c6551e82a1f57d06ffbc121eccdbaba94
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2025-02-07 19:59:05 +0200

    Schema-based json-structured output from Gemini response

--- lib/utils/geminiAI.ts
+++ lib/utils/geminiAI.ts
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from '@google/generative-ai'
+import { GoogleGenerativeAI, Schema, SchemaType } from '@google/generative-ai'
 import { LlmProcessOutput, LlmProcessProps, Strategy } from '../types'
 
 export const GEMINI_MODELS = {
@@ -8,12 +8,63 @@ export const GEMINI_MODELS = {
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY || '')
 
+const actionSchema: Schema = {
+    type: SchemaType.OBJECT,
+    properties: {
+        tokens: {
+            type: SchemaType.STRING,
+            description:
+                'Comma-separated list of symbols of the involved tokens, for example: USDC, ETH',
+            nullable: false
+        },
+        description: {
+            type: SchemaType.STRING,
+            description: 'Free text describing the action',
+            nullable: false
+        }
+    },
+    required: ['tokens', 'description']
+}
+
+const strategySchema: Schema = {
+    type: SchemaType.OBJECT,
+    properties: {
+        name: {
+            type: SchemaType.STRING,
+            description: 'Name of the strategy',
+            nullable: false
+        },
+        risk: {
+            type: SchemaType.STRING,
+            description: 'Risk level of the strategy',
+            nullable: false,
+            enum: ['low', 'medium', 'high']
+        },
+        actions: {
+            description: 'List of actions for the strategy',
+            type: SchemaType.ARRAY,
+            items: actionSchema
+        }
+    },
+    required: ['name', 'risk', 'actions']
+}
+
+const schema = {
+    description: 'List of strategies',
+    type: SchemaType.ARRAY,
+    items: strategySchema
+}
+
 export async function askGemini(llmInput: LlmProcessProps): Promise<LlmProcessOutput> {
     const prompt = JSON.stringify(llmInput.portfolio)
 
     try {
         const aiModel = genAI.getGenerativeModel({
-            model: GEMINI_MODELS.gemini20flashExp
+            model: GEMINI_MODELS.gemini20flashExp,
+            generationConfig: {
+                responseMimeType: 'application/json',
+                responseSchema: schema
+            }
         })
         const result = await aiModel.generateContent(prompt)
         // console.log(JSON.stringify(result))

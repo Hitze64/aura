2fa41f17cf7e776cf86b7f0494c35505cd5b2ef0
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2025-02-07 19:19:04 +0200

    Update build

---
+++ dist/utils/portfolio.d.ts
@@ -0,0 +1,19 @@
+import {
+    PortfolioForNetwork,
+    Strategy,
+    ProcessAddressProps,
+    LlmProcessProps,
+    AuraResponse_01
+} from '../types'
+export declare function getPortfolioForNetwork(
+    address: string,
+    networkId: string
+): Promise<import('ambire-common/dist/src/libs/portfolio/interfaces').PortfolioLibGetResult>
+export declare function getPortfolioVelcroV3(address: string): Promise<PortfolioForNetwork[]>
+export declare function llmMockProcess({ portfolio }: LlmProcessProps): Promise<Strategy[] | null>
+export declare const processAddress: ({
+    address,
+    getPortfolio,
+    llmProcessor
+}?: ProcessAddressProps) => Promise<AuraResponse_01>
+//# sourceMappingURL=portfolio.d.ts.map

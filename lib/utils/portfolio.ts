52cd0728cfc48a35b14b631f7d3c94f44f09fbd1
Cvetan Mihaylov <17980963+cmihaylov@users.noreply.github.com>
2025-04-24 14:08:53 +0300

    Fix: remove temp code in portofolio module

--- lib/utils/portfolio.ts
+++ lib/utils/portfolio.ts
@@ -12,54 +12,6 @@ import {
     NetworkPortfolioLibResponse
 } from '../types'
 
-// import { z } from 'zod'
-// import { ACTION_OPERATION_TYPES, RISK_TYPES } from '..'
-// import { zodResponseFormat } from 'openai/helpers/zod'
-
-//  const ActionZodSchema = z.object({
-//     tokens: z.string({
-//         description:
-//             'Comma-separated list of symbols of the involved crypto currencies or tokens, for example: USDC, ETH'
-//     }),
-//     description: z.string({
-//         description: 'Free text describing the action concerning the related tokens, the platform to use and expected APY'
-//     }),
-//     platforms: z.array(
-//         z.string({
-//             description: 'DeFi platform name'
-//         }),
-//         { description: 'The DeFi platform(s) which is to be used for the action' }
-//     ),
-//     networks: z.array(
-//         z.string({
-//             description: 'Lower-cased name of blockchain network'
-//         }),
-//         {
-//             description:
-//                 'The name of the blockchain network(s) which the action is to be executed on'
-//         }
-//     ),
-//     operations: z.array(z.enum(ACTION_OPERATION_TYPES, { description: 'DeFi operation type' }), {
-//         description: 'The DeFi operation type(s) used of the action'
-//     }),
-//     apy: z.string({
-//         description:
-//             'The annual yield that can be expected from this action. Example values: 3%, 5%, 8-10%'
-//     })
-// })
-
-//  const StrategyZodSchema = z.object({
-//     name: z.string({ description: 'Name of the strategy' }),
-//     risk: z.enum(RISK_TYPES, {
-//         description: 'Risk level of the strategy'
-//     }),
-//     actions: z.array(ActionZodSchema, { description: 'List of actions for the strategy' })
-// })
-
-//  const StrategiesZodSchema = z.object({
-//     strategies: z.array(StrategyZodSchema, { description: 'List of strategies' })
-// })
-
 export async function getPortfolioForNetwork(
     address: string,
     networkId: string

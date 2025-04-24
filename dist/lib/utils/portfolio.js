c6131ff60c3c09a308006794fea680bbb140e523
Cvetan Mihaylov <17980963+cmihaylov@users.noreply.github.com>
2025-04-24 14:09:18 +0300

    build: bump package.json version - v0.0.18

--- dist/lib/utils/portfolio.js
+++ dist/lib/utils/portfolio.js
@@ -11,50 +11,6 @@ const portfolio_1 = require("ambire-common/dist/src/libs/portfolio");
 const mockedAI_1 = require("./llm/mockedAI");
 const prompts_1 = require("./prompts");
 const strategies_1 = require("./strategies");
-// import { z } from 'zod'
-// import { ACTION_OPERATION_TYPES, RISK_TYPES } from '..'
-// import { zodResponseFormat } from 'openai/helpers/zod'
-//  const ActionZodSchema = z.object({
-//     tokens: z.string({
-//         description:
-//             'Comma-separated list of symbols of the involved crypto currencies or tokens, for example: USDC, ETH'
-//     }),
-//     description: z.string({
-//         description: 'Free text describing the action concerning the related tokens, the platform to use and expected APY'
-//     }),
-//     platforms: z.array(
-//         z.string({
-//             description: 'DeFi platform name'
-//         }),
-//         { description: 'The DeFi platform(s) which is to be used for the action' }
-//     ),
-//     networks: z.array(
-//         z.string({
-//             description: 'Lower-cased name of blockchain network'
-//         }),
-//         {
-//             description:
-//                 'The name of the blockchain network(s) which the action is to be executed on'
-//         }
-//     ),
-//     operations: z.array(z.enum(ACTION_OPERATION_TYPES, { description: 'DeFi operation type' }), {
-//         description: 'The DeFi operation type(s) used of the action'
-//     }),
-//     apy: z.string({
-//         description:
-//             'The annual yield that can be expected from this action. Example values: 3%, 5%, 8-10%'
-//     })
-// })
-//  const StrategyZodSchema = z.object({
-//     name: z.string({ description: 'Name of the strategy' }),
-//     risk: z.enum(RISK_TYPES, {
-//         description: 'Risk level of the strategy'
-//     }),
-//     actions: z.array(ActionZodSchema, { description: 'List of actions for the strategy' })
-// })
-//  const StrategiesZodSchema = z.object({
-//     strategies: z.array(StrategyZodSchema, { description: 'List of strategies' })
-// })
 async function getPortfolioForNetwork(address, networkId) {
     const network = networks_1.networks.find((n) => n.id === networkId);
     if (!network)
